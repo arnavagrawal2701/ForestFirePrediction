0f611d18f51b65acef3c2876ded746e10597dec3
cosminion27 <arnavagerawal2701@gmail.com>
2023-03-19 01:22:49 +0530

    Initial project commit

---
+++ Forest Fire Prediction Binary Classification Model.py
@@ -0,0 +1,28 @@
+#Import Prerequisites
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+#Import Data
+data=pd.read_csv('C:/Users/Arnav/Desktop/Arnav/Forest Fire Prediction/ForestFireDataset.csv')
+
+#Converting data into Train and Test data
+X=data.drop(['Area', 'Fire Occurrence'], axis=1)
+y = data['Fire Occurrence']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#Create a model
+model=LogisticRegression()
+
+#Train the model
+model.fit(X_train,y_train)
+
+#Run the test on the model
+y_pred = model.predict(X_test)
+
+#Test the model
+print('Accuracy:', accuracy_score(y_test, y_pred))
+print('Confusion Matrix:\n', confusion_matrix(y_test, y_pred))
+print('Classification Report:\n', classification_report(y_test, y_pred))
